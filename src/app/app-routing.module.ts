6a23617fc74401a961852a8a9f24fa4b26c88e22
Nithiya03 <nithiyasrirud03@gmail.com>
2022-04-13 17:32:06 +0530

    angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+   {path:'login/uty',component:LoginComponent},
+   {path:'**',component:NotFoundComponent},
+   { path: '', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
